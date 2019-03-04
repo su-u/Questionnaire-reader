b31e060d3e195867f2c43cb4d826c7aa2bf30c80
すー <aaaiiiuuu35@gmail.com>
2019-03-04 20:40:43 +0900

    api

--- Questionnaire reader/Questionnaire reader/Questionnaire_reader.py
+++ Questionnaire reader/Questionnaire reader/Questionnaire_reader.py
@@ -1,21 +1,43 @@
-import pyocr
-import pyocr.builders
-import argparse
-from PIL import Image
+import requests
+import base64
+import json
+import settings
 
-#parser = argparse.ArgumentParser(description='tesseract ocr test')
-#parser.add_argument('image', help='image path')
-#args = parser.parse_args()
+GOOGLE_CLOUD_VISION_API_URL = 'https://vision.googleapis.com/v1/images:annotate?key='
 
-tools = pyocr.get_available_tools()
+API_KEY = settings.AP
+#API_KEY = "AIzaSyB46NoyoqwjmTfpVLCabb1Tn_MAkoDsaj0"
 
-if len(tools) == 0:
-    print("No OCR tool found")
-    sys.exit(1)
-# The tools are returned in the recommended order of usage
-tool = tools[0]
+# APIを呼び、認識結果をjson型で返す
+def request_cloud_vison_api(image_base64):
+    api_url = GOOGLE_CLOUD_VISION_API_URL + API_KEY
+    req_body = json.dumps({
+        'requests': [{
+            'image': {
+                'content': image_base64.decode('utf-8') # jsonに変換するためにstring型に変換する
+            },
+            'features': [{
+                'type': 'TEXT_DETECTION', # ここを変更することで分析内容を変更できる
+                'maxResults': 10,
+            }]
+        }]
+    })
+    res = requests.post(api_url, data=req_body)
+    return res.json()
 
-res = tool.image_to_string(Image.open("SnapCrab_NoName_2019-2-16_2-43-51_No-00.png"),
-                           lang="jpn",
-                           builder=pyocr.builders.TextBuilder(tesseract_layout=6))
-print(res);
+# 画像読み込み
+def img_to_base64(filepath):
+    with open(filepath, 'rb') as img:
+        img_byte = img.read()
+    return base64.b64encode(img_byte)
+
+# 文字認識させたい画像を./img.pngとする
+img_base64 = img_to_base64('./img.png')
+result = request_cloud_vison_api(img_base64)
+
+#認識した文字の位置など、すべての情報を出力
+print("{}".format(json.dumps(result, indent=4)))
+
+#認識した文字のみを出力
+text_r = result["responses"][0]["fullTextAnnotation"]["text"]
+print(text_r)
